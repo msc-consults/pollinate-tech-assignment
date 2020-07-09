db42ed02acaed4a6e4e150a4216bac2f33e193a9
MChauhan <msc.consults@gmail.com>
2020-07-09 14:26:49 +0100

    introduced datetime injector api method

--- datetime-injector-app/app.py
+++ datetime-injector-app/app.py
@@ -1,27 +1,35 @@
 #!flask/bin/python
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, abort, make_response
+from datetime import datetime
 from confluent_kafka import Producer
 
 app = Flask(__name__)
 p = Producer({'bootstrap.servers': 'localhost:9092'})
-id = 1
+
 
 @app.route('/')
 def index():
-    global id
-    id += 1
-    key = 'hello-' + str(id)
-    message = 'world'
-
-    # TODO: serialise this to json, or de-serialise to json on comsumer end
-    p.produce('mytopic', key=key, value=message)
-    return jsonify({'Status': 'Message Produced', 'Key': key, "Value": message}), 201
+    return jsonify({'Message': 'Welcome to my datetime generator application'}), 200
 
 
 @app.route('/producer/flush', methods=['GET'])
 def flush_command():
     p.flush(30)
     return 'All messages have been produced'
+
+# TODO allow my api to accept messages
+@app.route('/producer/insert', methods=['POST'])
+def insert_message():
+    key = 'time'
+    dateTimeObj = str(datetime.now())
+    # TODO: de-serialise to json on comsumer end
+    p.produce('datetime-topic', key=key, value=dateTimeObj)
+    response = {
+        'Status': 'Message Produced',
+        'Message': {'Key': key, "Value": dateTimeObj}
+    }
+    return jsonify(response)
+
 
 @app.errorhandler(404)
 def not_found(error):
